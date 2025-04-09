521b8600c2aab4294d1ae33dd39c8dccca39d9aa
ADVAITHP <118455903+UROYHK@users.noreply.github.com>
2025-04-09 09:52:37 -0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,15 @@
 <?php
+$host = 'dpg-cvra8mq4d50c73al7340-a';
+$port = '5432';
+$dbname = 'php_login_db';
+$user = 'php_login_db_user';
+$password = 'BMQcKTDZsCp6ycFHsrO2Xm8pcHhLnKtV';
 
-$host="localhost";
-$user="root";
-$pass="";
-$db="login";
-$conn=new mysqli($host,$user,$pass,$db);
-if($conn->connect_error){
-    echo "Failed to connect DB".$conn->connect_error;
+try {
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "✅ Connected to PostgreSQL successfully!";
+} catch (PDOException $e) {
+    echo "❌ Connection failed: " . $e->getMessage();
 }
 ?>
