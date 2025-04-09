cdf73c2a10842ee81f765bb803b45cf7b39b941a
ADVAITHP <118455903+UROYHK@users.noreply.github.com>
2025-04-09 10:12:54 -0700

    Update register.php

--- register.php
+++ register.php
@@ -9,13 +9,13 @@ if(isset($_POST['signUp'])){
     $password=$_POST['password'];
     $password=md5($password);
 
-     $checkEmail="SELECT * From users where email='$email'";
+     $checkEmail="SELECT * From php-login-db where email='$email'";
      $result=$conn->query($checkEmail);
      if($result->num_rows>0){
         echo "Email Address Already Exists !";
      }
      else{
-        $insertQuery="INSERT INTO users (firstName, lastName, email, password)
+        $insertQuery="INSERT INTO php-login-db (firstName, lastName, email, password)
 VALUES ('Read', 'Write', 'readwrite53@gmail.com', md5('admin123'));
             if($conn->query($insertQuery)==TRUE){
 header("location: index.php");
@@ -34,7 +34,7 @@ if(isset($_POST['signIn'])){
    $password=$_POST['password'];
    $password=md5($password) ;
    
-   $sql="SELECT * FROM users WHERE email='$email' and password='$password'";
+   $sql="SELECT * FROM php-login-db WHERE email='$email' and password='$password'";
    $result=$conn->query($sql);
    if($result->num_rows>0){
     session_start();
